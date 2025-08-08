368710d31793f155fa5f76003ce587f75e0a8e65
psantacruz73 <potvin@cox.net>
2025-08-07 20:56:01 -0700

    KBs

---
+++ SimKBs/story_content/5qFKEoIw9eR_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5qFKEoIw9eR_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.102.35072.0%0D%0A%0D%0A00:00:00.064%20--%3E%2000:00:02.372%0D%0AChoose%20the%20scroll%20bar%20to%20continue%20scrolling%20down.%0D%0A%0D%0A"}]})
